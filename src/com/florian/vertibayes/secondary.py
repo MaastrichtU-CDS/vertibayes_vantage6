2a06f6eb46235390756537f9ffefeff010da38f3
Djura Smits <djura.smits@gmail.com>
2021-12-16 10:50:36 +0100

    Fixed subprocess call

--- src/com/florian/vertibayes/secondary.py
+++ src/com/florian/vertibayes/secondary.py
@@ -6,7 +6,7 @@ DEFAULT_PORT = 8888
 
 def RPC_init(client):
     # TODO: Create properties file
-    subprocess.run(f'java -jar {_get_jar_path()}')
+    subprocess.run(['java', '-jar', _get_jar_path()])
 
 
 def _get_jar_path():
