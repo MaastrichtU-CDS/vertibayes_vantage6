f7b482c319a0d1f2e750bb9ad12dd21ccf411185
Florian <florian.vandaalen@maastro.nl>
2022-07-19 14:40:32 +0200

    updated version

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-IMAGE=harbor.carrier-mu.src.surf-hosted.nl/carrier/vertibayes:1.0-stable
+IMAGE=harbor.carrier-mu.src.surf-hosted.nl/carrier/vertibayes:2.0
 
 docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" \
   --build-arg SSH-PUBLIC-KEY="$(cat ~/.ssh/id_rsa.pub)" \
