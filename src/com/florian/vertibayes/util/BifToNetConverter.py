795e2df2e9923526a7d43da5849f6586d4895dbf
florian <florian.vandaalen@maastro.nl>
2023-04-21 11:30:02 +0200

    added bif to net converter

---
+++ src/com/florian/vertibayes/util/BifToNetConverter.py
@@ -0,0 +1,64 @@
+import xmltodict
+
+def findStates(name, variables):
+    for var in variables['Variable']:
+        if var['@name'] == name:
+            return len(var["States"]['State'])
+
+def mapBif(bif):
+    dict = xmltodict.parse(bif)['ProbModelXML']['ProbNet']
+    net = "net{}";
+    for var in dict['Variables']['Variable']:
+        net += " node " + var['@name'] + " { states = ( "
+        for state in var["States"]['State']:
+            net += "\"" + state['@name'] + "\" "
+
+        net+=");}"
+
+    for pot in dict['Potentials']['Potential']:
+        net += "potential ( "
+        parents = []
+        if len(pot['Variables']['Variable']) == 1:
+            net += pot['Variables']['Variable']['@name']
+            name =  pot['Variables']['Variable']['@name']
+        else:
+            name =  pot['Variables']['Variable'][0]['@name']
+            count = 0
+            for var in pot['Variables']['Variable']:
+                if name != var['@name']:
+                    parents.append(var['@name'])
+                net+=var['@name']
+                if count < len(pot['Variables']['Variable'])-1:
+                    net += " "
+                if count == 0:
+                    net += "| "
+                count += 1
+        net+=    " ) {  data = "
+
+        potentials = pot['Values'].split(" ")
+        states = findStates(name, dict['Variables'])
+        for i in range(0, len(potentials)):
+            if i % states == 0:
+                    potentials[i] = "(" + potentials[i]
+            if i % states == states-1:
+                potentials[i] = potentials[i] + ")"
+        for parent in reversed(parents):
+            parentStates = findStates(parent, dict['Variables'])
+            states = states * parentStates
+            for i in range(0, len(potentials)):
+                if i % states == 0:
+                    potentials[i] = "(" + potentials[i]
+                if i % states == states-1:
+                    potentials[i] = potentials[i] + ")"
+        pot = ""
+        for i in range(0, len(potentials)):
+            pot+= potentials[i]
+            if(i < len(potentials)-1):
+                pot += " "
+        pot = pot.replace(") (", ")(")
+        net += pot
+        net+=" ;}"
+    return net
+
+
+
